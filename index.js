316bbb59b5c80ffa0f7e40f81dd9fcc5743f56ff
takumi-uezono0 <uezono1998@gmail.com>
2022-05-21 01:00:50 +0900

    create: index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+class Detail extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="classification-name">名前</div>
+        <div className="description">説明</div>
+        <div className="unit-price">0円</div>
+        <div className="num-people">
+          <select value="0">
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </select>
+          <span>名</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Summary extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="party">
+          <input type="text" className="party" value="0" />
+          <span>名様</span>
+        </div>
+        <div className="total-amount">
+          <span>合計</span>
+          <input type="text" className="total-amount" value="0" />
+          <span>円</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+class AdmissionFeeCalculator extends React.Component {
+  render() {
+    return (
+      <>
+        <Detail />
+        <Summary />
+      </>
+    );
+  }
+}
+
+const App: React.FC = () => {
+  return (
+    <div className="main">
+      <AdmissionFeeCalculator />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector(".container"));
